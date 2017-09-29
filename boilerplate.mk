ffa011abfba49b9a25bed287694666c10b67f519
Rowan Manning <rowan.manning@ft.com>
2017-09-29 10:51:43 +0100

    Simplify the boilerplate
    You now need to run an npm install before you can use the makefile

--- boilerplate.mk
+++ boilerplate.mk
@@ -3,7 +3,6 @@
 # This section of the Makefile should not be modified, it includes
 # commands from the Origami service Makefile.
 # https://github.com/Financial-Times/origami-service-makefile
-node_modules/%/index.mk: package.json ; npm install $* ; touch $@
--include node_modules/@financial-times/origami-service-makefile/index.mk
+include node_modules/@financial-times/origami-service-makefile/index.mk
 # [edit below this line]
 # ------------------------
